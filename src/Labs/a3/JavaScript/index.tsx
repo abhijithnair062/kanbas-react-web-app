77f2a9c3e9e0b718f787c76996d6895da8fe089c
Abhijith <abhijithnair062@gmail.com>
2024-02-19 14:54:06 -0500

    2.2.6.1 completed

---
+++ src/Labs/a3/JavaScript/index.tsx
@@ -0,0 +1,21 @@
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+import VariableTypes from "./variables/VariableTypes";
+import BooleanVariables from "./variables/BooleanVariables";
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import ES5Functions from "./functions/ES5Functions";
+function JavaScript() {
+    console.log("Hello World!");
+    return(
+       <div>
+          <h1>JavaScript</h1>
+          <VariablesAndConstants/>
+          <VariableTypes/>
+          <BooleanVariables/>
+          <IfElse/>
+          <TernaryOperator/>
+          <ES5Functions/>
+       </div>
+    );
+ }
+ export default JavaScript
