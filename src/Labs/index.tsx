be168fc6aa7753102b0636b490a2d7baf3c87cac
Abhijith <abhijithnair062@gmail.com>
2024-02-19 18:53:17 -0500

    2.3.2 completed

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,8 +1,14 @@
+// import {Link} from "react-router-dom";
 import Assignment3 from "./a3";
+import Nav from "../Nav";
 function Labs() {
     return (
        <div>
            {/* <h1>Assignment 3</h1>  */}
+         {/* <Link to="/Labs/a3">A3</Link> |
+         <Link to="/Kanbas">Kanbas</Link> |
+         <Link to="/hello">Hello</Link> | */}
+         <Nav/>
           <Assignment3/>
        </div>
     );
