462d70e7e87ad95f1b0c577dcde2a239e16030c1
Abhijith <abhijithnair062@gmail.com>
2024-02-19 19:20:20 -0500

    2.3 completed

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,15 +1,25 @@
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import Assignment3 from "./a3";
+import Assignment4 from "./a4";
 import Nav from "../Nav";
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
 function Labs() {
     return (
-       <div>
+       <div className="container-fluid">
            {/* <h1>Assignment 3</h1>  */}
          {/* <Link to="/Labs/a3">A3</Link> |
          <Link to="/Kanbas">Kanbas</Link> |
          <Link to="/hello">Hello</Link> | */}
+         <h1>Labs</h1>
          <Nav/>
-          <Assignment3/>
+         <Link to="/Labs/a3">Assignment 3</Link> |
+         <Link to="/Labs/a4">Assignment 4</Link>
+         <Routes>
+            <Route path="/a3/*" element={<Assignment3 />} />
+            <Route path="/a4" element={<Assignment4 />} />
+         </Routes>
+          {/* <Assignment3/> */}
        </div>
     );
  }
